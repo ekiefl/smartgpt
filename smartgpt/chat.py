528516429f5999ab5348ddc6fb35bf9ea11fb1ee
Evan Kiefl <kiefl.evan@gmail.com>
2023-05-12 11:52:07 -0700

    GPT-4 generated docstrings

--- smartgpt/chat.py
+++ smartgpt/chat.py
@@ -1,3 +1,18 @@
+"""This module is a wrapper around OpenAI's GPT model
+
+It defines two main classes, Agent
+and SmartGPT, which together facilitate interactive sessions with the AI model. The
+module supports different modes of interaction, including zero-shot, "step-by-step"
+chain of thought prompt flavoring, and the full "SmartGPT" resolver mode.
+
+Classes:
+    Agent:
+        Represents an interface to the GPT model.
+    SmartGPT:
+        Manages interactive sessions with the AI, supporting different modes of
+        interaction.
+"""
+
 from __future__ import annotations
 
 from typing import Dict, List, Tuple
@@ -17,6 +32,26 @@ GPT_PREFIX = REPLColors.OKBLUE + "\n" + REPLColors.ENDC
 
 @attrs.define
 class Agent:
+    """Represents an interface to the GPT model.
+
+    It encapsulates the process of sending
+    messages to the model and receiving responses. The class also handles token count
+    management and message history.
+
+    Attributes:
+        messages:
+            A list of messages sent to and received from the model.
+        credentials:
+            Credentials for accessing the model.
+        model:
+            The model to use (default is 'gpt-4').
+        temp:
+            The temperature parameter to use when generating responses.
+        token_counts:
+            A dictionary mapping the length of the message history to the total token
+            count.
+    """
+
     messages: List[Dict[str, str]] = attrs.field(factory=list)
     credentials: Credentials = attrs.field(factory=Credentials.default)
     model: str = attrs.field(default="gpt-4")
@@ -24,9 +59,24 @@ class Agent:
     token_counts: Dict[int, int] = attrs.field(factory=dict)
 
     def append_message(self, message: Message) -> None:
+        """Appends a message to the current message history.
+
+        Args:
+            message: The message to append.
+        """
         self.messages.append(attrs.asdict(message))
 
     def request(self) -> Response:
+        """Sends the current message history to the GPT model via an API request
+
+        The message history includes all previous interactions, which allows the model
+        to generate a response based on the entire conversation context.
+
+        Returns:
+            Response:
+                A Response object that encapsulates the model's response, which includes
+                the generated message, remaining tokens, and other metadata.
+        """
         return Response.from_openai_response(
             openai.ChatCompletion.create(
                 model=self.model,
@@ -36,6 +86,19 @@ class Agent:
         )
 
     def response(self, prompt: str) -> Message:
+        """Appends prompt to message history and sends request to the GPT model.
+
+        The model's response is then appended to the message history, and the total
+        tokens used is updated.
+
+        Args:
+            prompt:
+                The prompt to send to the model.
+
+        Returns:
+            Message:
+                The model's response encapsulated in a Message object.
+        """
         self.append_message(Message(Role.USER, prompt))
 
         try:
@@ -51,6 +114,29 @@ class Agent:
 
 @attrs.define
 class SmartGPT:
+    """Manages interactive sessions with the AI, supports different interaction modes.
+
+    In zero-shot mode, the user's prompts are sent directly to the model. In
+    step-by-step mode, prompts are flavored with the "step-by-step" chain of thought
+    prompt. In resolver mode, the "SmartGPT" mode is used, where multiple agents
+    generate responses, which are scrutinized by a researcher agent and then a final
+    response is chosen by a resolver agent.
+
+    Attributes:
+        config:
+            The configuration for the interaction.
+        main:
+            The main agent, responsible for sending prompts, receiving responses, and
+            most importantly, storing a linear chat history.
+        researcher:
+            An agent responsible for scrutinizing generator responses.
+        resolver:
+            An agent responsible for considering the researcher's analysis and creating
+            the final response.
+        generators:
+            A list of agents for generating responses in resolver mode.
+    """
+
     config: GPTConfig
     main: Agent
     researcher: Agent
@@ -58,16 +144,24 @@ class SmartGPT:
     generators: List[Agent]
 
     def repl(self) -> None:
-        """Starts the REPL"""
+        """Starts the Read-Eval-Print-Loop (REPL) for interaction with the model
+
+        This function runs indefinitely until the session is manually terminated.
+        """
         while True:
             _, _ = self.repl_iteration()
 
     def repl_iteration(self, quiet=False) -> Tuple[str, str]:
-        """A single Read-Eval-Print-Loop pass
+        """Executes a single pass of the Read-Eval-Print-Loop (REPL)
+
+        Args:
+            quiet:
+                If true, the function will not print the model's response. Default is
+                False.
 
         Returns:
-            prompt, reponse:
-                What the user typed and what the response was.
+            Tuple[str, str]:
+                The user's prompt and the model's response.
         """
         prompt = input(USER_PREFIX)
 
@@ -79,6 +173,29 @@ class SmartGPT:
         return prompt, response.content
 
     def create_response(self, prompt: str) -> Message:
+        """
+        Determines the mode of interaction based on the current configuration and
+        generates a response from the model accordingly.
+
+        In ZERO_SHOT mode, the prompt is sent directly to the main agent, which sends it
+        to the model and returns the response.
+
+        In STEP_BY_STEP mode, the prompt is transformed using the `prompts.step_by_step`
+        function before being sent to the model. The transformed prompt is intended to
+        guide the model to generate a more detailed response.
+
+        In RESOLVER mode, multiple generators create potential responses to the prompt.
+        The researcher agent points out the flaws in each generated response. Finally,
+        the resolver agent combines the selected responses into a final response.
+
+        Args:
+            prompt:
+                The prompt to send to the model.
+
+        Returns:
+            Message:
+                The model's response encapsulated in a Message object.
+        """
         if self.config.mode == Mode.ZERO_SHOT:
             response = self.main.response(prompt)
 
--- smartgpt/datatypes.py
+++ smartgpt/datatypes.py
@@ -1,3 +1,21 @@
+"""
+This module defines fundamental data types and configurations for interacting with
+the GPT model. It provides enums for interaction modes and roles, structures for
+messages, responses and configurations.
+
+Classes:
+    Mode:
+        Enum representing the different modes of interaction.
+    GPTConfig:
+        Configuration for the GPT model.
+    Role:
+        Enum representing the different roles in a conversation.
+    Message:
+        Structure for encapsulating a message in a conversation.
+    Response:
+        Structure for encapsulating a response from the GPT model.
+"""
+
 from __future__ import annotations
 
 import attrs
@@ -8,6 +26,8 @@ from smartgpt.credentials import Credentials
 
 
 class Mode(strenum.StrEnum):
+    """Enum representing the different modes of interaction."""
+
     ZERO_SHOT = strenum.auto()
     STEP_BY_STEP = strenum.auto()
     RESOLVER = strenum.auto()
@@ -15,16 +35,35 @@ class Mode(strenum.StrEnum):
 
 @attrs.define(frozen=True)
 class GPTConfig:
+    """Configuration for the GPT model.
+
+    Attributes:
+        model:
+            The GPT model to use (default is 'gpt-4').
+        credentials:
+            Credentials for accessing the model.
+        mode:
+            The mode of interaction.
+    """
+
     model: str = attrs.field(default="gpt-4")
     credentials: Credentials = attrs.field(factory=Credentials.default)
     mode: Mode = attrs.field(default=Mode.ZERO_SHOT)
 
     @classmethod
     def default(cls) -> GPTConfig:
+        """Factory method to create a default GPTConfig.
+
+        Returns:
+            GPTConfig:
+                A GPTConfig instance with default values.
+        """
         return cls()
 
 
 class Role(strenum.StrEnum):
+    """Enum representing the different roles in a conversation"""
+
     USER = strenum.auto()
     ASSISTANT = strenum.auto()
     SYSTEM = strenum.auto()
@@ -32,18 +71,48 @@ class Role(strenum.StrEnum):
 
 @attrs.define(frozen=True)
 class Message:
+    """Structure for encapsulating a message in a conversation.
+
+    Attributes:
+        role:
+            The role of the sender of the message.
+        content:
+            The content of the message.
+    """
+
     role: Role
     content: str
 
 
 @attrs.define
 class Response:
+    """Structure for encapsulating a response from the GPT model.
+
+    Attributes:
+        message:
+            The message from the model.
+        total_tokens:
+            The total number of tokens used.
+        finish_reason:
+            The reason for finishing the response.
+    """
+
     message: Message
     total_tokens: int
     finish_reason: str
 
     @classmethod
     def from_openai_response(cls, response) -> Response:
+        """Factory method to create a Response from an OpenAI API response.
+
+        Args:
+            response:
+                The response from the OpenAI API.
+
+        Returns:
+            Response:
+                A Response instance with values from the API response.
+        """
         return cls(
             message=cattrs.structure(response["choices"][0]["message"], Message),
             total_tokens=response["usage"]["total_tokens"],
