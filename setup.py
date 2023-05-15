d795d0faff3ee1fcb9079edb7be16196979d22ba
Evan Kiefl <kiefl.evan@gmail.com>
2023-05-14 17:52:41 -0700

    Add the setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="smartgpt",
+    version="0.1.0.dev2",
+    packages=find_packages(),
+    scripts=["bin/smartgpt"],
+    author_email="kiefl.evan@gmail.com",
+    author="Evan Kiefl",
+    url="https://github.com/ekiefl/smartgpt",
+    install_requires=[
+        "openai",
+        "attrs",
+        "cattrs",
+        "pyyaml",
+        "prompt_toolkit",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
